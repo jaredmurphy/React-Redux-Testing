5f1634dbfac523e1f067bbdbd382a001a7bc10f1
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-12 03:08:50 -0500

    adds styling for form & button

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -21,11 +21,14 @@ class CommentBox extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+        <h4> Add a Comment </h4>
         <textarea 
           value={this.state.comment} 
           onChange={this.handleChange.bind(this)} 
          />
-        <button>Submit Comment</button>
+         <div>
+          <button>Submit Comment</button>
+         </div>
       </form>
     );
   }
