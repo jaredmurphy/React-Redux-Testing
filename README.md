e9642f2cc197b409c89aa21e482f3c94df847360
Jared Murphy <jaredcmurphy@gmail.com>
2016-11-12 02:17:04 -0500

    adds tests for comments

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 More about testing not necessarily super-interesting React Components. Simple features to focus on testing. 
 
 These specs just assert that the end-product, the HTML that gets rendered on the page, is going to be rendered correctly. These specs do not test any specific methods and just ensure that the tests are flexible, allow other developers to refactor this code as much as possible. Tests should only fail when features break. 
+
+   
+       
+    
+        
--- test/components/comment_box.js
+++ test/components/comment_box.js
@@ -2,16 +2,38 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class', () => {
+    expect(component).to.have.class('comment-box');
+  });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
+  });
 
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea')
+        .simulate('change', 'This is a comment');
+    });
+
+    it('shows that text in the text area', () => {
+      expect(component.find('textarea')).to.have.value('This is a comment');
+    });
+
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
   });
 });
 
